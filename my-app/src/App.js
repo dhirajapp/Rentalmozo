2395f87396675e7f145f398c170dc4d4fca03242
Dhiraj Kumar <dhirajkumar31298@gmail.com>
2022-07-01 13:10:34 +0530

    project setup

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <img src={logo} className="App-logo" alt="logo" /> 
+       <h1>Hello flok's</h1>
+      <h2>Welcome to Rentalmozo !</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
